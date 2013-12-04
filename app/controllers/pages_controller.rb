47ec2406633aa03ecd6776924d6f938f0d766ea1
bangarang <me@alexjrock.com>
2013-12-04 00:08:43 -0600

    inventory.txt location and sign_up removal

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,7 @@ class PagesController < ApplicationController
 	end
 
 	def items
-		text = File.read('public/inventory.txt').gsub(/\"/,'')
+		text = File.read('inventory.txt').gsub(/\"/,'')
 		@things = []
 		CSV.parse(text, { :headers => true, header_converters: :symbol, :col_sep => "\t" }) do |row|
 			@things.push(row.to_hash)
