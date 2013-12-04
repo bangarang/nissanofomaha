503344c13709c705d9c3207e4c2be3e705dde564
bangarang <me@alexjrock.com>
2013-12-04 00:16:01 -0600

    inventory.txt link location

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,10 @@ class PagesController < ApplicationController
 	before_filter :authorize
 	
 	def home
-		text = File.read('public/inventory.txt').gsub(/\"/,'')
-		@things = []
-		CSV.parse(text, { :headers => true, :col_sep => "\t" }) do |row|
-			@things.push(row.to_hash)
-		end
-
-		@stuff = @things.first
 	end
 
 	def items
-		text = File.read('inventory.txt').gsub(/\"/,'')
+		text = File.read('public/inventory.txt').gsub(/\"/,'')
 		@things = []
 		CSV.parse(text, { :headers => true, header_converters: :symbol, :col_sep => "\t" }) do |row|
 			@things.push(row.to_hash)
