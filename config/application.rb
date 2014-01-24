711ac95258eeb1fec1d1d17abca2001b9a93a877
bangarang <me@alexjrock.com>
2014-01-24 09:18:47 -0600

    Nissan AG Font

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Vauto
     require 'csv'
 
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.assets.precompile += %w( .svg .eot .woff .ttf )
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
