e99eb8d6115608d4342f8a26b126923fcdddce76
bangarang <me@alexjrock.com>
2014-04-01 14:19:02 -0500

    CarChat24

--- config/routes.rb
+++ config/routes.rb
@@ -65,6 +65,8 @@ Vauto::Application.routes.draw do
 
   get 'usedvehicles' => 'vehicles#index'
 
+  get "chat" => 'pages#test'
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
